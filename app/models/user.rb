cad3fc8a1da3ff2c092b17515d0a934b73fff39c
Jared Smith <jaredsmithse@icloud.com>
2013-10-05 19:08:16 -0400

    Add unimplemented #update_reviews and #get_reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def update_reviews
+  
+  end
+  
+  def get_reviews
+
+  end
 end
