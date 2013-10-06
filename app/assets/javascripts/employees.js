d9e2d264620388439e2822bfc620928ae5a6682d
Jared Smith <jaredsmithse@icloud.com>
2013-10-06 02:22:09 -0400

    Add js to deal with adding and displaying new employees

---
+++ app/assets/javascripts/employees.js
@@ -0,0 +1,30 @@
+$(document).ready( function() {
+	var newEmployee = "	<form class='new-employee-submit'><input id='name' type='text' placeholder='Name'><input id='date' type='date'><input id='from' type='time'><input id='to' type='time'><input type='submit' class='btn' value='Submit'></form>"
+	
+	$('.new-employees').on('click', function(e) {
+		e.preventDefault();
+		$('.employee-content').html(newEmployee)
+	})
+
+	$('body').on('submit', '.new-employee-submit', function(e) {
+		e.preventDefault();
+		
+		var name = $('#name').val()
+		var date = $('#date').val()
+		var from = $('#from').val()
+		var to = $('#to').val()
+
+		$.ajax({
+			type: 'POST',
+			url: '/employees/new',
+			data: { 'name': name,
+							'date': date,
+							'from': from,
+							'to': to }
+		}).done(updateEmployees);
+	});
+
+	function updateEmployees(response) {
+		$('.employees').html(response);
+	}
+})
