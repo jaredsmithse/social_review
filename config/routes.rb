a2b23231b5d781a2f0359d472e126ff2806a4e6b
Jared Smith <jaredsmithse@icloud.com>
2013-10-06 02:25:27 -0400

    Add route for creating an employee

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ SocialReview::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+  post '/employees/new', to: 'employees#new'
 end
