f16ef9bcd13b6663c5e37108027edaa58b4abf1a
Jared Smith <jaredsmithse@icloud.com>
2013-10-06 03:56:16 -0400

    Add calls to setup the csv file on load

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,7 +4,12 @@ class EmployeesController < ApplicationController
   def new
   	parse_employee_data(params['name'], params['date'], params['from'], params['to'])
   	@user = current_user
+  	Employee.build_report(@user.id)
   	render 'employees/_employees', layout: false
+  end
+
+  def charts
+  	render :charts
   end
 
   def parse_employee_data(name, date, from, to)
@@ -12,8 +17,8 @@ class EmployeesController < ApplicationController
 		employee = Employee.find_or_create_by_name(name)
 		puts "CHECKPOINT ONE"
 		current_user.employees << employee
-		shift.start_time = from
-		shift.end_time = to
+		shift.start_time = date + " " + from
+		shift.end_time = date + " " + to
 		puts "CHECKPOINT TWO"
 		employee.shifts << shift
 		employee.save
