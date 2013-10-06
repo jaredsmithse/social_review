451fcdd2170ff0eae6aa13f6932177452a92daba
Jared Smith <jaredsmithse@icloud.com>
2013-10-06 03:56:36 -0400

    Add method to update the csv file

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,14 @@
+require 'csv'
+
 class Employee < ActiveRecord::Base
 	has_and_belongs_to_many :shifts
 	belongs_to :user
+
+	def self.build_report(user_id)
+		CSV.open("./public/users/data.csv", "wb") do |csv|
+  		Employee.where(user_id: user_id).each do |employee|
+  			csv << [employee.name, employee.reviews.where(sentiment: "positive").count, employee.reviews.where(sentiment: "negative").count, employee.reviews.where(sentiment: "neutral").count]
+  		end
+		end
+	end
 end
