14a4006a8c6ee861cd9d23a8002f67a38d9cef59
Jared Smith <jaredsmithse@icloud.com>
2013-10-06 05:26:43 -0400

    Write code to handle the csv before graph generation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,10 +5,27 @@ class Employee < ActiveRecord::Base
 	belongs_to :user
 
 	def self.build_report(user_id)
+		employees = Employee.where(user_id: user_id)
+		employee_info = Hash.new(Hash.new())
+
+
+		Review.where(user_id: user_id).each do |review|
+			employees.each do |employee|
+				employee.shifts.each do |shift|
+					review_time = Time.at(review.date.to_i)
+					if  review_time > shift.start_time && review_time < shift.end_time
+						employee_info[employee][review.sentiment] += 1
+					end
+				end
+			end
+		end
+		
+
 		CSV.open("./public/users/data.csv", "wb") do |csv|
-  		Employee.where(user_id: user_id).each do |employee|
-  			csv << [employee.name, employee.reviews.where(sentiment: "positive").count, employee.reviews.where(sentiment: "negative").count, employee.reviews.where(sentiment: "neutral").count]
-  		end
+			csv << ["Name", "Positive Reviews", "Negative Reviews", "Neutral Reviews"]
+			employee_info.each do |employee, data|
+				csv << [employee.name, data['positive'], data['negative'], data['neutral']]
+			end
 		end
 	end
 end
